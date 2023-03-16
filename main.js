5e5986fbfe681b338b3523519c9953a33c08ce9c
AlfinhiHD <alfinhihd@gmail.com>
2023-03-16 11:55:59 +0700

    push js day 2

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
 const book = (
-    {name:"Seni untuk bersikap bodo amat", price:"90000", author:"Mark Manson"}
+    {name:"Seni untuk bersikap bodo amat", price:"100000", author:"Mark Manson", stock:"10"}
 );
 
-function bookpurchase (name, indc, intax) {
+function bookpurchase (book, indc, intax, many) {
     let result = 0;
 
-    const price = book.price
-    const dc = price * (indc/100);
-    const dcprice = price - dc;
-    const tax = dcprice * (intax/100);
-    const total = dcprice + tax;
+    let stock = book.stock;
+    
+    if(many <= stock) {
+        for(let i=0; i<many; i++) {
+       
+            if( book && (stock > 0)) {
+    
+                stock--;
+    
+                const price = book.price;
+                const dc = price * (indc/100);
+                const dcprice = price - dc;
+                const tax = dcprice * (intax/100);
+                const total = dcprice + tax;
+                
+                result += total;
+            }
+        }
+            console.group();
+            console.log("");
+            console.log("Jumlah buku yang dibeli : ",  many)
+            console.log("Harga total : ", result )
+            console.groupEnd();
+    } else {
+        console.log("Stock buku ini tidak mencukupi");
+    }
 
-    console.group();
-    console.log('Original Price', price);
-    console.log('Discount Amount', dc);
-    console.log('Price after Discount', dcprice);
-    console.log('Tax Amount', tax);
-    console.log('Price after Tax', total);
-    console.groupEnd();
+    return result;
 }
 
-bookpurchase(book, 25, 10);
-    
+bookpurchase(book, 25, 10, 8);
